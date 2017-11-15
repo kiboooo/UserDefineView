6e50005b26accee66ce186684e1ca2461e435e08
kiboooo <kiboooo78@gmail.com>
2017-11-15 20:59:20 +0800

    实现了自定义滑动转换图片

--- app/src/main/java/com/example/kiboooo/userdefinedview/MainActivity.java
+++ app/src/main/java/com/example/kiboooo/userdefinedview/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.kiboooo.userdefinedview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.kiboooo.userdefinedview.View.ImageBarnnerViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageBarnnerViewGroup mGroup;
+
+    private int[] ids = new int[]{
+            R.drawable.first_hanshake,
+            R.drawable.second_hanshake,
+            R.drawable.thirt_hanshake
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGroup = (ImageBarnnerViewGroup) findViewById(R.id.image_group);
+
+        for (int i = 0; i < ids.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(ids[i]);
+            mGroup.addView(imageView);
+        }
     }
 }
--- app/src/main/java/com/example/kiboooo/userdefinedview/View/ImageBarnnerViewGroup.java
+++ app/src/main/java/com/example/kiboooo/userdefinedview/View/ImageBarnnerViewGroup.java
@@ -2,6 +2,7 @@ package com.example.kiboooo.userdefinedview.View;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,9 @@ public class ImageBarnnerViewGroup extends ViewGroup {
     private int ChildrenCount; //ViewGroup中所有的子视图个数
     private int Childrenheight;//子视图的高
     private int Childrenwidth;//子视图的宽
+
+    private int first_x;//代表第一次按下时的X坐标；以及每一次移动过程中，移动之前的位置横坐标
+    private int index = 0;//代表图片的索引值；
 
     public ImageBarnnerViewGroup(Context context) {
         super(context);
@@ -108,16 +112,64 @@ public class ImageBarnnerViewGroup extends ViewGroup {
         return super.onInterceptTouchEvent(ev);
     }
 
+
+    /**
+     * 常用2种方式实现 轮播图的手动轮播
+     * 1.利用scrollTo，scrollBy 配合实现轮播图的手动轮播
+     * 2.利用 scroller 对象 实现；
+     *
+     *  （1）：滑动屏幕图片的过程中，就是自定义的ViewGroup的子视图的移动过程，那么需要知道滑动之前的
+     *  横坐标和滑动之后的横坐标，求出滑动的距离，利用 scrollBy 方法实现图片的滑动；
+     *
+     *  （2）： 求出按下那一瞬间的那个点的横坐标值作为我们的滑动前的横坐标初始值；
+     *
+     *  （3）：在不断的滑动过程中，系统不断的调用ACTION_MOVE 方法，我们需要将滑动之前的点 和 滑动之后的点
+     *  进行保存；
+     *
+     *  （4）： 在用户手指抬起的一瞬间，需要计算出已经滑动到哪张图片的位置，
+     *  需要求出滑动到的图片的索引值：
+     * （ 我们当前ViewGroup的滑动位置 + 我们每一张图片宽度 / 2 ）/ 每张图片的宽度值 。
+     * 再调用 scrollTo 方法 滑动到该图片上；
+     *
+     * @param event
+     * @return
+     */
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN://表示用户点击屏幕 按下的一瞬间
-
+                first_x = (int) event.getX();
+                Log.e("ACTION_DOWN", "" + first_x);
                 break;
             case MotionEvent.ACTION_MOVE://表示  按下后在屏幕上移动的过程
+                int MoveX = (int) event.getX();
+                int Distance = MoveX - first_x;//移动前后横坐标的变化范围
 
+                scrollBy(-Distance, 0);
+                //scrollBy：在当前视图内容继续偏移(x , y)个单位，
+                // 显示(可视)区域也跟着偏移(x,y)个单位，
+                // 由于 当手指向右（→）滑动的时候，MoveX > first_x
+                // 需要显示该图片前面的内容才能造成图片往后退的效果；
+                //所以需要进行偏移量取反的操作；左划（←）同理
+
+                first_x = MoveX;//把当前的移动结束的坐标作为下一次滑动前的横坐标；
+                Log.e("ACTION_MOVE", "" + Distance);
                 break;
             case MotionEvent.ACTION_UP://表示  用户从屏幕抬起的一瞬间
+                int ScrollX = getScrollX();//获取：该视图内容相当于视图起始坐标的偏移量
+
+                index = (ScrollX + Childrenwidth / 2) / Childrenwidth;
+
+                if (index < 0) {//表明已经滑动到最左端
+                    index = 0;
+                } else if (index > ChildrenCount - 1) {//表明已经滑动到最右端
+                    index = ChildrenCount - 1;
+                }
+
+                scrollTo(index * Childrenwidth, 0);
+
+                Log.e("ACTION_UP", "" + index);
                 break;
 
             default:
